fdcd1d34e3a6fa9c57a4e5f2fe0f6c107a9e0981
Amirhosein <ahaghazade@gmail.com>
2023-11-18 18:23:18 +0330

    Login/Building/Room/Adds fixed with arduinoJson library

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,244 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <WiFiMulti.h>
+
+WiFiMulti wifiMulti;
+// wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+
+const char* WifiSsid = "MobinNet20";
+const char* WifiPassword = "K6YJScyY";
+ 
+
+#define LOGIN_URL     "http://185.110.190.226:4001/api/users/login"
+#define BUILDING_URL  "http://185.110.190.226:4001/api/buildings"
+#define ROOM_URL      "http://185.110.190.226:4001/api/rooms?buildingId="
+#define ADD_URL       "http://185.110.190.226:4001/api/addresses?buildingId="
+
+#define LOGIN_USER    "root"
+#define LOGIN_PASS    "123"
+#define BUILDING_ID   "6542276fea75d4a5df95a7d0"
+#define ROOM_ID       "6549e908ea75d4a5df95ad2a"
+#define LEDPIN BUILTIN_LED
+
+// DynamicJsonDocument RoomsConf(200);
+// DynamicJsonDocument BuildingAddsConf(200);
+// DynamicJsonDocument RoomsAdd(200); // room all addresses
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+String Token = "";
+//---------------------------v-----------------
+//WIFI & Connections Functions
+//---------------------------*-----------------
+void WifiConnect(String SSID , String PASS , int LedPin)
+{
+  if(wifiMulti.run() != WL_CONNECTED) {
+  Serial.println("WiFi not connected!");
+  digitalWrite(LedPin , !digitalRead(LedPin));
+  delay(500);
+  }
+  digitalWrite(LedPin , LOW);
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+//---------------------------v-----------------
+//APP Functions
+//---------------------------*-----------------
+String PostReq(String URL ,String Reqdata,  String token)
+{
+  WiFiClient client;
+  HTTPClient http;
+  String response = "";
+  
+  http.begin(client, URL.c_str());  
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Authorization", token);
+
+  int httpResponseCode = http.POST(Reqdata);
+  if (httpResponseCode > 0) 
+  {
+    Serial.print("HTTP POST Response code: ");
+    Serial.println(httpResponseCode);
+    response = http.getString();
+  } 
+  else 
+  {
+    Serial.print("[POST] Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+
+  return response;
+}
+
+String GetReq(String URL , String token)
+{
+  WiFiClient client;
+  HTTPClient http;
+  String payload = "";
+  http.begin(client, URL);
+  http.addHeader("Authorization", token);
+
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == 200) {
+    payload = http.getString();
+  } else 
+  {
+    Serial.print("[GET] Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+  return payload;
+}
+
+bool Login(String User , String Pass)
+{
+  DynamicJsonDocument LoginInput(200);
+  LoginInput["username"] = User;
+  LoginInput["password"] = Pass;
+  String LoginReq = "";
+  serializeJson(LoginInput, LoginReq);
+  String LoginRes = PostReq(LOGIN_URL ,LoginReq, Token);
+
+  StaticJsonDocument<16> filter;
+  filter["token"] = true;
+  DynamicJsonDocument ResObj(256);
+  DeserializationError error = deserializeJson(ResObj, LoginRes,DeserializationOption::Filter(filter));
+  Serial.println();
+  serializeJsonPretty(ResObj, Serial);
+  Serial.println();
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return 0;
+  }
+
+  if(ResObj.size() == 0)
+  {
+    Serial.println("Result is empty");
+    return 0;
+  }
+  Token = ResObj["token"].as<String>();
+  return 1;
+}
+
+void Biulding(String URL)
+{
+  String BuildingRes = GetReq(URL, Token);
+  StaticJsonDocument<64> filter;
+  JsonObject filter_buildings_0 = filter["buildings"].createNestedObject();
+  filter_buildings_0["_id"] = true;
+  filter_buildings_0["name"] = true;
+
+  StaticJsonDocument<1000> BuildingResObj;
+  DeserializationError error;
+  do
+  {
+    DeserializationError error = deserializeJson(BuildingResObj, BuildingRes, DeserializationOption::Filter(filter));
+    Serial.println();
+    serializeJsonPretty(BuildingResObj, Serial);
+    Serial.println();
+     if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    delay(300);
+    }
+
+  } while (error);
+}
+
+String GetConf(String URL, String BuildingID, int JsonCapacity , StaticJsonDocument<300> Filter)
+{
+  DeserializationError error;
+  DynamicJsonDocument ConfResObj(JsonCapacity);
+  do
+  {
+    ConfResObj.clear();
+    String ConfRes = GetReq(URL + BuildingID, Token);
+    DeserializationError error = deserializeJson(ConfResObj, ConfRes, DeserializationOption::Filter(Filter));
+    Serial.println();
+    serializeJsonPretty(ConfResObj, Serial);
+    Serial.println();
+     if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    delay(300);
+    }
+
+  } while (error);
+
+  String FilteredRes = "";
+  serializeJson(ConfResObj, FilteredRes);
+
+  return FilteredRes;
+}
+
+String CalReadAdss(String RoomID , String RoomsConfigs, int RoomJsonSize, String RoomsAddConfigs, int AddsJsonSize)
+{
+  //RoomsConfigs --> {"_id": "6549e908ea75d4a5df95ad2a", "addresses": ["6549e8bbea75d4a5df95ad11", "6549e937ea75d4a5df95ad3c" ] }
+  //RoomsAddConfigs -->  {"_id": "6549e937ea75d4a5df95ad3c", "values": [],"format": "1 Byte (Unsigned)", "writeTo": "", "readFrom": "0/0/3","rangeType": "temprature"}
+  DynamicJsonDocument RoomsConfJson(RoomJsonSize);
+  deserializeJson(RoomsConfJson, RoomsConfigs);
+  DynamicJsonDocument AddsConfJson(AddsJsonSize);
+  deserializeJson(AddsConfJson, RoomsAddConfigs);
+
+  // Access the JSON object
+  Serial.println("Start Parsing Json");
+  Serial.println(RoomsConfJson["roooms"].size());
+  Serial.println(RoomsConfJson["roooms"].as<String>());
+  // for(int roomnum = 0 ; roomnum < RoomsConfJson["roooms"].size() ; roomnum++)
+  // {
+  //   Serial.println(RoomsConfJson["roooms"][roomnum].as<String>());
+  // }
+  return "Null";
+}
 
 void setup() {
-  // Initialize Serial port
   Serial.begin(115200);
-  while (!Serial) continue;
-
-  // StaticJsonDocument<200> doc;
-  DynamicJsonDocument  doc(200);
-
-  // Add values in the document
-  doc["sensor"] = "gps";
-  doc["time"] = 1351824120;
-  doc["_id"] = "asd456sdqwer";
-
-  // Add an array.
-  JsonArray data = doc.createNestedArray("values");
-  data.add(48.756080);
-  data.add(2.302038);
-
-  // Generate the minified JSON and send it to the Serial port.
-  //
-  serializeJson(doc, Serial);
-  // The above line prints:
-  // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
-
-  Serial.println();
-  serializeJsonPretty(doc, Serial);
-  // The above line prints:
-  // {
-  //   "sensor": "gps",
-  //   "time": 1351824120,
-  //   "values": [
-  //     48.756080,
-  //     2.302038
-  //   ]
-  // }
-  Serial.println();
-  int Value1 = doc["values"][0];//48
-  Serial.println(Value1);
+  pinMode(LED_BUILTIN, OUTPUT);
+  wifiMulti.addAP("MobinNet20", "K6YJScyY");
+  wifiMulti.addAP("Irancell-TF-i60-B6A7_1", "@tm@1425#@tm@");
+  WifiConnect(WifiSsid, WifiPassword , LEDPIN);
+  delay(1000);
+  //------------ GET CONFIGS FROM SERVER -----------------
+  //Wait until login success
+  while(!Login(LOGIN_USER,LOGIN_PASS)){delay(500);Serial.println("Attempting to LOGIN...");}
+  Serial.println("\n---------------\nTry GET Buildings Confing...");
+  Biulding(BUILDING_URL);
+  // Wait until Get rooms conf
+  StaticJsonDocument<64> roomfilter;
+  JsonObject filter_rooms_0 = roomfilter["rooms"].createNestedObject();
+  filter_rooms_0["_id"] = true;
+  filter_rooms_0["addresses"] = true;
+  String TotalRoomsConf = "";
+  while(TotalRoomsConf == ""){Serial.println("\n---------------\nTry GET Rooms Confing...");TotalRoomsConf = GetConf(ROOM_URL, BUILDING_ID,15000 , roomfilter);delay(500);}
+  // //Wait until Get addresses conf
+  StaticJsonDocument<240> Addfilter;
+  JsonObject filter_addresses_0 = Addfilter["addresses"].createNestedObject();
+  filter_addresses_0["_id"] = true;
+  filter_addresses_0["format"] = true;
+  filter_addresses_0["writeTo"] = true;
+  filter_addresses_0["readFrom"] = true;
+  filter_addresses_0["rangeType"] = true;
+  JsonObject filter_addresses_0_values_0 = filter_addresses_0["values"].createNestedObject();
+  filter_addresses_0_values_0["read"] = true;
+  filter_addresses_0_values_0["readFormat"] = true;
+  filter_addresses_0_values_0["readValueRangeType"] = true;
+  filter_addresses_0_values_0["write"] = true;
+  filter_addresses_0_values_0["writeFormat"] = true;
+  filter_addresses_0_values_0["writeValueRangeType"] = true;
+  String TotalAddConf = "";
+  while(TotalAddConf == ""){Serial.println("\n---------------\nTry GET Adds Confing...");TotalAddConf = GetConf(ADD_URL, BUILDING_ID, 15000, Addfilter);delay(500);}
   
+  Serial.println("\n========Create Readble Adds with Configs========\n");
+  CalReadAdss(ROOM_ID, TotalRoomsConf, 1500, TotalAddConf, 1500);
 }
 
 void loop() {
-  // not used in this example
+  sleep(100);
 }
