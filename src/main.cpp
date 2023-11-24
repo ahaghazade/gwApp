1487c29634b3cfb305bca7ffc5d93b48c225491b
ahaghazade <ahaghazade@gmail.com>
2023-11-24 23:39:14 +0330

    Good example for arduinojson

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,59 @@
-#include <Arduino_JSON.h>
-
-void objRec(JSONVar myObject) {
-  Serial.println("{");
-  for (int x = 0; x < myObject.keys().length(); x++) {
-    if ((JSON.typeof(myObject[myObject.keys()[x]])).equals("object")) {
-      Serial.print(myObject.keys()[x]);
-      Serial.println(" : ");
-      objRec(myObject[myObject.keys()[x]]);
-    }
-    else {
-      Serial.print(myObject.keys()[x]);
-      Serial.print(" : ");
-      Serial.print(myObject[myObject.keys()[x]]);
-      Serial.println(",");
-    }
-  }
-  Serial.println("}");
-}
-
-void valueExtractor() {
-
-  Serial.println("object");
-  Serial.println("======");
-  JSONVar myObject;
-  JSONVar add;
-  add[0] = "123456";
-  add[1] = "123456";
-  // Making a JSON Object
-  myObject["foo"] = "bar";
-  myObject["blah"]["abc"] = add;
-  myObject["blah"]["efg"] = "pod";
-  myObject["blah"]["cde"]["pan1"] = "King";
-  myObject["blah"]["cde"]["pan2"] = 3.14;
-  myObject["jok"]["hij"] = "bar";
-
-  Serial.println(myObject);
-  Serial.println();
-  Serial.println("Extracted Values");
-  Serial.println("======");
-
-  objRec(myObject);
-
-}
+#include <ArduinoJson.h>
 
 void setup() {
-
+  // Initialize Serial port
   Serial.begin(115200);
-  while (!Serial);
-  valueExtractor();
+  while (!Serial) continue;
 
+  // Allocate the JSON document
+  //
+  // Inside the brackets, 200 is the RAM allocated to this document.
+  // Don't forget to change this value to match your requirement.
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<200> doc;
+
+  // StaticJsonObject allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  // DynamicJsonDocument  doc(200);
+
+  // Add values in the document
+  //
+  doc["sensor"] = "gps";
+  doc["time"] = 1351824120;
+
+  JsonObject room = doc.createNestedObject("room");
+  room["1/1/1"] = "1Bit";
+  room["1/1/2"] = "2Bit";
+  // Add an array.
+  //
+  JsonArray data = doc.createNestedArray("listData");
+  data.add(48.756080);
+  data.add(2.302038);
+
+  // Generate the minified JSON and send it to the Serial port.
+  //
+  serializeJson(doc, Serial);
+  // The above line prints:
+  // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
+
+  // Start a new line
+  Serial.println();
+
+  // Generate the prettified JSON and send it to the Serial port.
+  //
+  serializeJsonPretty(doc, Serial);
+  // The above line prints:
+  // {
+  //   "sensor": "gps",
+  //   "time": 1351824120,
+  //   "data": [
+  //     48.756080,
+  //     2.302038
+  //   ]
+  // }
 }
 
 void loop() {
+  // not used in this example
 }
-
-
-
